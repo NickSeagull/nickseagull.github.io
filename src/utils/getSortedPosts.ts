e2cd5367ba9857b0dee7015ff9de5da82f2b69f6
Nikita Tchayka <nikitatchayka@gmail.com>
2023-08-06 14:06:56 +0100

    Remove rendered frontmatter

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -1,12 +1,12 @@
 import type { CollectionEntry } from "astro:content";
 
-const getSortedPosts = (posts: CollectionEntry<"blog">[]) =>
+const getSortedPosts = (posts: any[]) =>
   posts
-    .filter(({ data }) => !data.draft)
+    .filter(data => !data.draft)
     .sort(
       (a, b) =>
-        Math.floor(new Date(b.data.pubDatetime).getTime() / 1000) -
-        Math.floor(new Date(a.data.pubDatetime).getTime() / 1000)
+        Math.floor(new Date(b.published_at).getTime() / 1000) -
+        Math.floor(new Date(a.published_at).getTime() / 1000)
     );
 
 export default getSortedPosts;
